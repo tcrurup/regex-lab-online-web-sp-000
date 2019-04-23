632e37ceddd929324f7f49e906d23550310f36ae
Anthony Rurup <tcrurup@gmail.com>
2019-04-23 20:18:25 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,31 @@
 def starts_with_a_vowel?(word)
-
+  if(word.match(/\b[aeiou]/i))
+    true
+  else
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\bun\w*ing/i)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b[a-z]{5}\b/i)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if(text.match(/\A[A-Z].*[.?!]\z/))
+    true
+  else
+    false
+  end
 end
 
 def valid_phone_number?(phone)
-
+  if(phone.match(/\d{3}.?\d{3}.?\d{4}/))
+    true
+  else
+    false
+  end
 end
