d0b3e634d579405ff25b5b659f2501536716ccda
Anthony Rurup <tcrurup@gmail.com>
2019-04-23 20:12:05 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if(text.scan(/[A-Z]\w*[.?!]/))
+  if(text.match(/[A-Z]\w*[.?!]/))
     true
   else
     false
