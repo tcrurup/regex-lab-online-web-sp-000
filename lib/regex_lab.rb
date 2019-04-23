250b0b73944dea226268cb48a5b75e00b8946b5e
Anthony Rurup <tcrurup@gmail.com>
2019-04-23 19:59:50 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,17 @@
 def starts_with_a_vowel?(word)
-
+  if(word.match(/\b[aeiou]/i))
+    true
+  else
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\bun\w*ing/i)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/w{5})
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
