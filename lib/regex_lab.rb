5d03725ef71f7ba9019bd3a0768a1107b4f46888
Anthony Rurup <tcrurup@gmail.com>
2019-04-23 20:18:12 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,5 +23,9 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if(phone.match(/))
+  if(phone.match(/\d{3}.?\d{3}.?\d{4}/))
+    true
+  else
+    false
+  end
 end
